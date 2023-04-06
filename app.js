191b695dca593414d5a57333ef03ca9f250e4007
krishreddie <cheemarlaashwanthreddy@gmail.com>
2023-04-05 21:50:56 -0500

    update

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var NIKERouter = require('./routes/NIKE');
+var nikeRouter = require('./routes/nike');
 var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
+
+
+
+
 var app = express();
 
 // view engine setup
@@ -22,8 +27,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/NIKE', NIKERouter);
+app.use('/nike', nikeRouter);
 app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/selector.js
+++ routes/selector.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('nike', { title: 'Search Results of nike' });
+  res.render('selector', { title: 'selector' });
 });
 
 module.exports = router;
